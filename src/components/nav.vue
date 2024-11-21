76ac819c2feef34f74a9169ba3a400b754df853b
cassidycheesman <157512218+cassidycheesman@users.noreply.github.com>
2024-11-21 23:24:09 +0000

    components and libraries added

---
+++ src/components/nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#">Cassidy Cheesman</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#projects">Projects</b-nav-item>
+        <b-nav-item href="#research">Research</b-nav-item>
+        <b-nav-item href="#about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.navbar-brand {
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 2rem;
+}
+
+.b-nav-item {
+  font-family: Arial, sans-serif;
+  font-size: 1rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NavBar from '@/components/nav.vue'
+</script>
+
+<template>
+  <main>
+    <NavBar />
+  </main>
+</template>
