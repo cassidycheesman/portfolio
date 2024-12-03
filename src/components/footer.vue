322dbb36efdb0c4aa113f2f15ed62926a0355470
cassidycheesman <157512218+cassidycheesman@users.noreply.github.com>
2024-12-03 00:55:10 +0000

    added final homepage content and footer

---
+++ src/components/footer.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="icons">
+      <a href="mailto:cassidycheesman@gmail.com">
+        <img src="/images/icon_email.png" alt="Email">
+      </a>
+      <a href="https://linkedin.com/in/cassidy-cheesman">
+        <img src="/images/icon_linkedin.png" alt="LinkedIn">
+      </a>
+      <a href="https://www.youtube.com/@cassidycheesman445">
+        <img src="/images/icon_youtube.png" alt="YouTube">
+      </a>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 3rem;
+}
+
+.icons {
+  display: flex;
+  gap: 5rem; /* Adjust the gap between icons as needed */
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <script setup>
+import { ref } from 'vue'
+const slideInterval = ref(5000)
 import NavBar from '@/components/nav.vue'
+import FootBar from '@/components/footer.vue'
 </script>
 
 <template>
@@ -42,16 +45,51 @@ import NavBar from '@/components/nav.vue'
         <h2 class="about-heading">
           About Me<span class="title-period-reverse">.</span>
         </h2>
+        <h3 class="about-sub">
+          I am a jack of all trades, master of some.
+        </h3>
         <p>
-          I am a jack of all trades, master of some. Through my diverse experiences and toolbelt of skills, I am able to approach problems from a unique perspective and create innovative solutions. I am passionate about working with others to find new solutions to old problems.
+          Through my diverse experiences and toolbelt of skills, I am able to approach problems from a unique
+          perspective and create innovative solutions. I am passionate about working with others to find new solutions
+          to old problems.
         </p>
         <p>
-          The superpower I bring to the table is my positive energy and ability to bring people together. Every problem has a solution, we may just not have the right minds on it yet. I am here to help bring those minds together and create something amazing.
+          The superpowers I bring to the table are my positive energy, curiosity, and ability to bring people together.
+          Every problem has a solution, we may just not have the right minds on it yet. I am here to help bring those
+          minds together and create something amazing.
         </p>
       </div>
-      <img class="portrait" src="/public/images/portrait.jpg" alt="A photo of Cassidy Cheesman">
+      <img class="portrait" src="/images/portrait.jpg" alt="A photo of Cassidy Cheesman">
+    </div>
+
+    <div class="favproj">
+      <div class="carousel-title">
+        <h2 class="about-heading">
+          Favorite Projects<span class="title-period">.</span>
+        </h2>
+        <p>
+          A quick look at some of my favorite projects I have worked on. My projects range in disciplines - from fine art to mechanical design to full stack development - but all include a deep understanding of the user, creative touches, and a problem solved. Head to my projects page to see more!
+        </p>
+      </div>
+      <div class="carousel">
+        <BCarousel :interval="slideInterval" controls indicators ride="carousel" class="projcarousel">
+          <BCarouselSlide img-src="/images/littleman1.png">
+            <h1 class = "carousel-heading">Running Automaton</h1>
+          </BCarouselSlide>
+          <BCarouselSlide img-src="/images/cmachine.jpg">
+            <h1 class = "carousel-heading">Automated Cocktail Machine</h1>
+          </BCarouselSlide>
+          <BCarouselSlide img-src="/images/prisonerexpress.png">
+            <h1 class = "carousel-heading2">Prisoner Express PWA</h1>
+          </BCarouselSlide>
+        </BCarousel>
+      </div>
     </div>
   </main>
+
+  <footer>
+    <FootBar />
+  </footer>
 </template>
 
 <style scoped>
@@ -65,7 +103,7 @@ import NavBar from '@/components/nav.vue'
 }
 
 .title-period {
-  color: rgb(44, 118, 192);
+  color: rgb(88, 116, 141);
   /* Change this to the desired color */
 }
 
@@ -102,7 +140,7 @@ import NavBar from '@/components/nav.vue'
 .me {
   display: flex;
   justify-content: center;
-  background-color: rgb(93, 141, 188);
+  background-color: rgb(88, 116, 141);
   /* Change this to the desired color */
 }
 
@@ -117,13 +155,12 @@ import NavBar from '@/components/nav.vue'
 
 .title-period-reverse {
   color: white;
-  /* Change this to the desired color */
 }
 
 .about-heading {
   font-family: 'Times New Roman', Times, serif;
   font-size: 4rem;
-  margin-bottom:2vh;
+  margin-bottom: 2vh;
 }
 
 .textabout {
@@ -140,5 +177,48 @@ import NavBar from '@/components/nav.vue'
   /* Adjust the size as needed */
   text-align: right;
   width: 40vw;
+}
+
+.about-sub {
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 2rem;
+  margin-bottom: 2.5vh;
+}
+
+.favproj {
+  margin-top: 13vw;
+  margin-bottom: 11vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.carousel-title {
+  text-align: end;
+  margin-right: 3vw;
+  width: 35vw;
+}
+
+.carousel-title p {
+  font-family: 'Trebuchet MS', sans-serif;
+  font-size: 1.3rem;
+}
+
+.projcarousel {
+  width: 45vw;
+}
+
+.carousel-heading {
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 2.5rem;
+  color: white;
+  text-decoration: underline;
+}
+
+.carousel-heading2 {
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 2.5rem;
+  color: black;
+  text-decoration: underline;
 }
 </style>
