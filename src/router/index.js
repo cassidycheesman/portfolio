6f7964159148febb6c9ad822a61dad809240cc98
cassidycheesman <157512218+cassidycheesman@users.noreply.github.com>
2025-04-15 20:59:20 +0000

    whoops, dropped the router for research

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WIProgress from '../views/WIP.vue'
+import Research from '../views/Research.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,7 +19,7 @@ const router = createRouter({
     {
       path: '/research',
       name: 'research',
-      component: WIProgress,
+      component: Research,
     },
     {
       path: '/about',
