ca2e9a6afa546d092a58fb2e8f9f68adea4e8955
cassidycheesman <157512218+cassidycheesman@users.noreply.github.com>
2025-04-15 21:27:24 +0000

    touched up research, gotta resize media and arrange

--- src/views/Research.vue
+++ src/views/Research.vue
@@ -15,16 +15,6 @@ import FootBar from '@/components/footer.vue'
                 Research <span class="title-period-second">.</span>
             </h1>
 
-            <div class="seam">
-            <h2 class="rtitle">
-                SeamPose
-            </h2>
-
-            <h3 class="rdescription">
-                Repurposing Seams as Capacitive Sensors in a Shirt for Upper-Body Pose Tracking
-            </h3>
-            </div>
-
             <vue-plyr>
                 <video controls crossorigin playsinline data-poster="poster.jpg">
                     <source :src="`${baseDir}videos/SeamPoseVideo.mp4`" type="video/mp4" />
@@ -59,25 +49,4 @@ import FootBar from '@/components/footer.vue'
     color: rgb(0, 0, 0);
 }
 
-.seam{
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.rtitle {
-    font-family: 'Times New Roman', Times, serif;
-    font-size: 2.5rem;
-    /* Adjust the size as needed */
-    font-weight: bold;
-    color: rgb(255, 255, 255);
-}
-
-.rdescription {
-    font-family: 'Times New Roman', Times, serif;
-    font-size: 2.5rem;
-    /* Adjust the size as needed */
-    font-weight: bold;
-    color: rgb(255, 255, 255);
-}
 </style>
